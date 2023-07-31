48fbe6fb06d9833da9eddf3b43f3e2452395d1c3
Mrig26 <mrigendrasoni26@gmail.com>
2023-07-31 07:31:43 -0700

    adding stuff

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+## Foundry Solidity Course Repo
---
+++ foundry-fund-me-f23/test/FundMeTest.t.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Idnetifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "../lib/forge-std/src/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundme;
+
+    function setUp() external {
+        fundme = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    }
+
+    function testDemo() public {
+        assertEq(fundme.MINIMUM_USD(), 5e18);
+    }
+}
