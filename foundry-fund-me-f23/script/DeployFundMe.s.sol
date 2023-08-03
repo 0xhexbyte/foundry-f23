a521500f3971b7095f8bc5556fca7da07767b618
Mrig26 <mrigendrasoni26@gmail.com>
2023-08-02 18:48:33 -0700

    adding stuff

--- foundry-fund-me-f23/script/DeployFundMe.s.sol
+++ foundry-fund-me-f23/script/DeployFundMe.s.sol
@@ -3,11 +3,15 @@ pragma solidity ^0.8.13;
 
 import {Script} from "../lib/forge-std/src/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.sol";
 
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+        // After startBroadcast() real tx!
         vm.startBroadcast();
-        FundMe fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
         return fundMe;
     }
--- foundry-fund-me-f23/script/HelperConfig.sol
+++ foundry-fund-me-f23/script/HelperConfig.sol
@@ -4,7 +4,29 @@ pragma solidity ^0.8.18;
 import {Script} from "../lib/forge-std/src/Script.sol";
 
 contract HelperConfig {
+    NetworkConfig public activeNetworkConfig;
 
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        // price feed address\
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {}
 }
 
 // 1. Deploy mocks when we are on a local chain
